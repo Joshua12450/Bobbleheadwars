99013bbe7bfeceef05a384735415427d85c7f81a
Joshua12450 <Joshua12450@gmail.com>
2023-10-14 00:44:30 -0400

    chapter 6 done marine walking anim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private CharacterController characterController;
     public LayerMask layerMask;
     private Vector3 currentLookTarget = Vector3.zero;
+    public Animator bodyAnimator;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,11 +30,12 @@ public class PlayerController : MonoBehaviour
  0, Input.GetAxis("Vertical"));
         if (moveDirection == Vector3.zero)
         {
-            // TODO
+            bodyAnimator.SetBool("IsMoving", false);
         }
         else
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
+            bodyAnimator.SetBool("IsMoving", true);
         }
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
